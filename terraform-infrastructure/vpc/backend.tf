0444106541bc8af6cfe797ff4b37d4f05677a693
Jakub Szuber <90647840+JakubSzuber@users.noreply.github.com>
2023-08-27 23:42:40 +0200

    Update backend.tf [skip actions]

--- terraform-infrastructure/vpc/backend.tf
+++ terraform-infrastructure/vpc/backend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "golden-devops-bucket-demo"
+    bucket          = "golden-devops-bucket"
     #key    = "k8-demo-vpc.tfstate"
     #key    = "env:/${terraform.workspace}/k8-demo-vpc.tfstate"  # TODO
     key    = "vpc.tfstate"
-    region = "us-east-1"
+    region          = "us-east-1"
+    dynamodb_table  = "golden-devops-dynamodb"
+    encrypt         = true
   }
 }
