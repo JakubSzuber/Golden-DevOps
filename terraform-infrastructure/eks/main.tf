1fde774d62bc6a7ed683bf88b686ef356247fb0b
Jakub Szuber <jszuber06@gmail.com>
2023-10-05 23:03:25 +0200

    Update eks/main.tf

--- terraform-infrastructure/eks/main.tf
+++ terraform-infrastructure/eks/main.tf
@@ -68,7 +68,7 @@ module "eks" {
       type        = "ingress"
       self        = true
     }
-    ingress_all = { # TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+    ingress_all = { # TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
       description      = "Node all ingress"
       protocol         = "-1"
       from_port        = 0
