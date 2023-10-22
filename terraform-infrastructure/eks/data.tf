41fab25bdeeb7fef4d0751a0a75d2763c2e25af8
Jakub Szuber <jszuber06@gmail.com>
2023-10-22 16:34:24 +0200

    Fix bug with aws auth [skip actions]

--- terraform-infrastructure/eks/data.tf
+++ terraform-infrastructure/eks/data.tf
@@ -11,11 +11,3 @@ data "terraform_remote_state" "vpc" {
     encrypt        = true
   }
 }
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
--- terraform-infrastructure/eks/outputs.tf
+++ terraform-infrastructure/eks/outputs.tf
@@ -11,9 +11,5 @@ output "cluster_certificate_authority_data" {
 }
 
 output "cluster_id" {
-  value = module.eks.cluster_id
-}
-
-output "cluster_name" {
-  value = var.cluster_name
+  value = module.eks.cluster_name
 }
